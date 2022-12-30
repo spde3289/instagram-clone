ef5260d5a4f981e4161f19dc1449d98a629a416d
spde3289 <spde3289@naver.com>
2022-12-30 18:34:09 +0900

    Update LoginPage.js

--- src/page/LoginPage.js
+++ src/page/LoginPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import smartPhoneImg from '../imgs/smartphone.png';
+import LogoImg from '../imgs/Logo.png';
 
 const LoginPage = () => {
 
@@ -8,31 +10,37 @@ const LoginPage = () => {
             <SmartPhone>
                 
             </SmartPhone>
-            <MainBox>
-                <Logo>
-                    <div className='Logo'></div>
-                </Logo>
-                <LoginBox>
-                    <IdBox/>
-                    <PaswardBox/>
-                    <LoginButton>
+            <div className="Ass">
+                <MainBox>
+                    <Logo>
+                        <div></div>
+                    </Logo>
+                    <LoginBox>
+                        <IdBox/>
+                        <PaswardBox/>
+                        <LoginButton>
+                            <div>
+                                로그인
+                            </div>
+                        </LoginButton>
                         <div>
-                            로그인
+                            또는
                         </div>
-                    </LoginButton>
-                    <div>
-                        또는
-                    </div>
-                    <FacebookLogin>
-                        <FacebookImg>
-                            
-                        </FacebookImg>
-                        <FacebookText>
-                            Facebook으로 로그인
-                        </FacebookText>
-                    </FacebookLogin>
-                </LoginBox>
-            </MainBox>
+                        <FacebookLogin>
+                            <FacebookImg>
+                                
+                            </FacebookImg>
+                            <FacebookText>
+                                Facebook으로 로그인
+                            </FacebookText>
+                        </FacebookLogin>
+                        <ForgotPassword>
+                            비밀번호를 잊어셨나요?
+                        </ForgotPassword>
+                    </LoginBox>
+                </MainBox>
+                <SignUpBox></SignUpBox>
+            </div>
         </Main>
     );
 };
@@ -41,14 +49,20 @@ const Main = styled.main`
     display: flex;
     justify-content: center;
     align-items: center;
+    margin-top: 32px;
+    .Ass{
+        display: flex;
+        height: 727px;
+        align-items: center;
+        flex-direction: column;
+        justify-content: center;
+    }
 `;
 
 const SmartPhone = styled.div`
-    width: 380.32px;
-    height: 601px;
-    background-image: url(https://static.cdninstagram.com/rsrc.php/v3/y4/r/ItTndlZM2n2.png);
-    background-size: 468.32px 634.15px;
-    background-position: -46px 639px;
+    width: 380.312px;
+    height: 581.15px;
+    background: url(${smartPhoneImg}) no-repeat -46px 0px;
     margin-right: 32px;
     margin-bottom: 12px;
 `;
@@ -62,20 +76,23 @@ const MainBox = styled.div`
     height: 379.984px;
     padding: 10px 0;
     border: 1px solid #000;
+    margin-bottom: 10px;
 `;
 
 const Logo = styled.div`
-    background-image: url(https://static.cdninstagram.com/rsrc.php/v3/y-/r/yXM3FgMdVNX.png);
-    background-position: 0px -52px;
+    background-image: url(${LogoImg}); //https://static.cdninstagram.com/rsrc.php/v3/y-/r/yXM3FgMdVNX.png
+    background-position: 0px -52px; 
     background-size: auto;
     width: 175px;
     height: 51px;
-    background-repeat: no-repeat;
-    display: inline-block;
+    margin: 36px 0 12px;
 `;
 
 const LoginBox = styled.form`
-    width: 100px;
+    display: flex;
+    width: 348px;
+    height: 266.984px;
+    flex-direction: column;
 `;
 
 const IdBox = styled.input`
@@ -96,6 +113,16 @@ const FacebookImg = styled.span`
 `;
 
 const FacebookText = styled.span`
+`;
+
+const ForgotPassword = styled.a`
+
+`;
+
+const SignUpBox = styled.div`
+    width: 348px;
+    height: 41px;
+    border: 1px solid black;
 `;
 
 export default LoginPage;
