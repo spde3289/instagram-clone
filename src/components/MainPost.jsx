69d35f271bf165f01c3775d5d8b4968c0be163fc
spde3289 <47699011+spde3289@users.noreply.github.com>
2023-01-07 06:11:46 +0900

    Create MainPost.jsx

---
+++ src/components/MainPost.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MainPost = () => {
+
+    return(
+        <>
+            <StoryHeader>
+
+            </StoryHeader>
+            메인페이지
+        </>
+    );
+};
+
+const StoryHeader = styled.div`
+    
+`; 
+
+export default MainPost;
