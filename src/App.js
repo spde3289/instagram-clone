1b0cad7b271cb25521e9e3ea568c0af4781b35d0
spde3289 <spde3289@naver.com>
2023-01-09 13:59:41 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-//import LoginPage from './page/LoginPage';
-import MainPage from "./page/MainPage";
+import LoginPage from './page/LoginPage';
+// import MainPage from "./page/MainPage";
 import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <MainPage/>,
+    element: <LoginPage/>,
     errorElement: <ErrorPage />,
   },
 ]);
 
 function App() {
+
+
+
+
   return (
       <RouterProvider router={router} />
   );
