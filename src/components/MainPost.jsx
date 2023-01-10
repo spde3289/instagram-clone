f27c4cab2e97d8da803d17649cfc11a9d18992df
spde3289 <spde3289@naver.com>
2023-01-11 07:28:01 +0900

    Update MainPost.jsx

--- src/components/MainPost.jsx
+++ src/components/MainPost.jsx
@@ -5,16 +5,59 @@ const MainPost = () => {
 
     return(
         <>
-            <StoryHeader>
-
-            </StoryHeader>
+            <PostBox>
+                <Post>
+                    <PostHeader>
+                        <Profile>
+                            프로필 사진
+                        </Profile>
+                        <Option>    
+                            더보기 아이콘
+                        </Option>
+                    </PostHeader>  
+                    <div>
+                        <PostImg></PostImg>
+                        
+                    </div>
+                </Post>
+            </PostBox>
             메인페이지
         </>
     );
 };
 
-const StoryHeader = styled.div`
+const PostBox = styled.div`
+    width: 268px;
+    height:85px;
+    padding: 16px 0;
+    margin-top: 27px;
+    border: 1px solid #cccccc;
+    border-radius: 8px;
+    background: #ffffff;
+`;
+
+const Post = styled.div`
     
-`; 
+`;
+
+const PostHeader = styled.div`
+    
+`;
+
+const Profile = styled.div`
+
+`;
+
+const Option = styled.div`
+
+`;
+
+const PostImg = styled.div`
+
+`;
+
+const Comment = styled.div`
+
+`;
 
 export default MainPost;
