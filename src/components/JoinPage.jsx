84aa3b036b0d006d1e52ffd3cce2e226541a96df
spde3289 <spde3289@naver.com>
2023-01-13 21:04:55 +0900

    Update JoinPage.jsx

--- src/components/JoinPage.jsx
+++ src/components/JoinPage.jsx
@@ -15,42 +15,71 @@ const JoinPage = (props) => {
             onClick={()=>{
                 handlePopUp()
             }}/>
+            <Title>회원가입</Title> 
             <Join>
-                아이디 
-                <JoinId/>
+                아이디<div>중복 검사</div>
+                <JoinInput/>
+
                 비밀번호
-                <JoinPassword/>
+                <JoinInput/>
                 이름
-                <JoinName/>
+                <JoinInput/>
+
+                <Summit>가입하기</Summit>
             </Join>
         </JoinBox>
     );
 };
 
 const JoinBox = styled.div`
-    position: absolute;
-    border: 1px solid black;
-    background: #fff;
+    position: relative;
+    box-sizing: border-box;
+    width: 350px;
+    height: 475px;
+    top: -423px;
+    left: -1px;
+    border: 1px solid #cccccc;
+    background: #ffffff;
+    padding: 25px 20px;
     .icon{
         position: absolute;
-        right: 0px;
+        right: 25px;
         width: 24px;
-        height: 24px;
-    }
+        height: 32px;
+    };
+    .text{
+
+    };
+`;
+
+const Title = styled.h2`
+    font-weight: normal;
+
 `;
 
 const Join = styled.form`
     display: flex;
     flex-direction: column;
+    margin: 20px 15px;
 `;
 
-const JoinId = styled.input`
+const JoinInput = styled.input`
+    border: 1px solid #cccccc;
+    background: rgba(216,216,216,0.171);
+    border-radius: 5px;
+    margin-bottom: 30px;
+    padding: 9px 0 7px 8px;
+    outline: none;
 `;
 
-const JoinPassword = styled.input`
+const Summit = styled.button`
+    height: 32px;
+    border: none;
+    background-color: rgb( 0,149,246);
+    border-radius: 8px;
+    color: white;
+    opacity: .7;
 `;
 
-const JoinName = styled.input`
-`;
 
 export default JoinPage;
