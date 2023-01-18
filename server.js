b6b82431e4b903cfc98cd80c29752de6058e8c34
spde3289 <spde3289@naver.com>
2023-01-18 11:30:12 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
-const jsonSever = require('json-sever');
-const sever = jsonSever.create();
-const router = jsonSever.require('./data.json');
-const middlewares = jsonSever.defaults({
-    static : './build',
-});
-const port = process.env.PORT || 3000;
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-sever.use(middlewares);
-sever.use(
-    jsonSever.rewriter({
-        "/api/*": '/$1',
-    })
-);
+server.use(middlewares)
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
 
-sever.use(router);
-
-sever.listen(port, ()=>{
-    console.log('sever is running');
-});
-
+// Export the Server API
+module.exports = server
