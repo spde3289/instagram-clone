de163a5637ffc86498a035c1a115942aeb666ba3
spde3289 <spde3289@naver.com>
2023-02-15 01:51:57 +0900

    Update error-page.jsx

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
@@ -13,4 +14,4 @@ export default function ErrorPage() {
       </p>
     </div>
   );
-}
+} 
